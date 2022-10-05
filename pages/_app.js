3cce8566f5f618844c1cd9138eebb05375b14d21
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-10-05 21:15:35 +0100

    Handle refresh token

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 
 import { CognitoJwtVerifier } from "aws-jwt-verify";
-import { getCookie as nextGetCookie } from "cookies-next";
+import {
+  getCookie as nextGetCookie,
+  setCookie as nextSetCookie,
+} from "cookies-next";
 
 const COOKIE_NAME = "ACCESS_TOKEN";
+const REFRESH_COOKIE_NAME = "REFRESH_TOKEN";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
@@ -22,6 +26,7 @@ MyApp.getInitialProps = async ({ ctx, router }) => {
 
 const isAuthorised = async ({ req, res }) => {
   const token = nextGetCookie(COOKIE_NAME, { req, res });
+  const refreshToken = nextGetCookie(REFRESH_COOKIE_NAME, { req, res });
 
   if (!token) {
     return null;
@@ -29,6 +34,28 @@ const isAuthorised = async ({ req, res }) => {
 
   const verifier = createVerifier();
   const payload = await tryParseToken(token, verifier);
+
+  if (payload !== null) return payload;
+
+  if (refreshToken === null) return null;
+
+  const newTokens = await tryRefreshTokens(refreshToken);
+
+  if (newTokens !== null) {
+    console.log("Access token refreshed");
+
+    nextSetCookie(COOKIE_NAME, newTokens.access_token, {
+      req,
+      res,
+      path: "/",
+      sameSite: "lax",
+      httpOnly: true,
+    });
+
+    const payload = await tryParseToken(newTokens.access_token, verifier);
+
+    return payload;
+  }
 
   return payload;
 };
@@ -58,6 +85,40 @@ const redirect = (req, res, path) => {
     Location: path,
   });
   res.end();
+};
+
+const tryRefreshTokens = async (refresh_token) => {
+  const refreshUrl = `${process.env.NEXT_PUBLIC_COGNITO_URI}/oauth2/token`;
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const redirectUri = process.env.NEXT_PUBLIC_DOMAIN;
+  const secret = process.env.CLIENT_SECRET;
+
+  return new Promise(async (resolve) => {
+    const res = await fetch(refreshUrl, {
+      method: "POST",
+      headers: new Headers({
+        "content-type": "application/x-www-form-urlencoded",
+      }),
+      body: Object.entries({
+        grant_type: "refresh_token",
+        client_id: clientId,
+        redirect_uri: redirectUri,
+        refresh_token: refresh_token,
+        client_secret: secret,
+      })
+        .map(([k, v]) => `${k}=${v}`)
+        .join("&"),
+    });
+
+    if (!res.ok) {
+      resolve(null);
+      return;
+    }
+
+    const newTokens = await res.json();
+
+    resolve(newTokens);
+  });
 };
 
 export default MyApp;

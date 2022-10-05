518928d0dcc6b72d7f6df285daa0ed2c4459104f
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-10-05 19:30:56 +0100

    Update Login Page

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,14 @@
 export default function Login() {
+  const loginUrl = `${process.env.COGNITO_URI}/login?client_id=${process.env.CLIENT_ID}&response_type=code&scope=email+openid+phone&redirect_uri=${process.env.DOMAIN}/api/auth`;
+
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Login Page</h1>
 
       <p>
-        <a href="">Click me</a>
+        <a href={loginUrl}>Login</a>
       </p>
     </div>
   );
 }
+

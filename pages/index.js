fecdb565003cc98a13914877a2d2782e45a2c9c9
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2022-10-07 14:20:48 +0100

    Update logout page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 export default function Home() {
+  const handleLogout = () => {
+    const url = `/api/revoke`;
+
+    var requestOptions = {
+      method: "GET",
+      headers: [],
+      redirect: "follow",
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => response.text())
+      .then(() => {
+        Router.push("/login");
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div>
       <h1>Protected Homepage</h1>
 
       <div>
-        <button>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
